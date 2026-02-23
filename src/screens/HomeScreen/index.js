70155f71c5940253929183bb6387b8ab46d14ef6
Betül Sağlam <149970112+betulsaglam@users.noreply.github.com>
2026-02-23 17:11:36 +0300

    feat: implement home screen ui (#7)
    moved example carditem components to progress screen for now cause why not

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,44 +1,109 @@
-import { Text, View } from 'react-native';
-import Button from '../../components/Button';
+import React from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import CardItem from '../../components/CardItem';
+import Button from '../../components/Button';
 
 const HomeScreen = ({ navigation }) => {
+  const insets = useSafeAreaInsets();
+
+  // mock attributes and methods according to lld
+  const userProfile = {
+    username: 'bbamtest'
+  }
+
+  const workoutPlans = [
+    { id: '1', name: 'My Daily Workout', totalSteps: 5, avgDuration: 45 },
+    { id: '2', name: 'Legs', totalSteps: 4, avgDuration: 20 },
+    { id: '3', name: 'Arms', totalSteps: 3, avgDuration: 15 },
+    { id: '4', name: 'Test 1', totalSteps: 5, avgDuration: 45 },
+    { id: '5', name: 'Test 2', totalSteps: 4, avgDuration: 20 },
+    { id: '6', name: 'Test 3', totalSteps: 3, avgDuration: 15 },
+  ];
+
+  const totalWorkouts = 6;
+  const totalTimeSpent = 5;
+
+  const loadWorkoutPlans = () => {};
+  const loadUserProfile = () => {};
+  const navigateToCreateWorkout = () => {};
+  const navigateToWorkoutDetails = () => {};
+
   return (
-    <View className='flex-1 gap-4 px-6 items-center justify-center bg-bbam-back-page text-m3-label-large font-normal'>
-      <Text>Home Screen</Text>
+    <View className="flex-1 bg-bbam-back-page" style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View className="flex-1 px-6 pt-10">
+        
+        {/* Header Section */}
+        <View className="flex-row justify-between items-center mb-10">
+          <View className='ml-2'>
+            <Text className="text-m3-headline-medium text-bbam-text-main">
+              {`Hi, ${userProfile.username}`}
+            </Text>
+            <Text className="text-m3-label-large text-bbam-text-main">
+              Ready for a workout?
+            </Text>
+          </View>
+          <View className="w-14 h-14 bg-bbam-back-card rounded-[20px] items-center justify-end overflow-hidden">
+            <Ionicons name="person" size={48} color="#585AD1" className='-mb-2' />
+          </View>
+        </View>
 
-      <Text className='text-m3-headline-small'>Example Card Items!!</Text>
-      <CardItem
-        variant='workoutDisplay'
-        title='My Daily Workout'
-        subtitle='5 steps, 45 mins'
-        onPress={() => console.log('pressed workout plan')}
-      />
-      <CardItem
-        variant='exerciseDisplay'
-        title='Squat'
-        subtitle='12 Reps'
-      />
-      <CardItem
-        variant='exerciseAdd'
-        title='Push-Up'
-        onAdd={() => console.log('pressed add exercise')}
-      />
-      <CardItem
-        variant='exerciseEdit'
-        title='Daily Workout'
-        subtitle='16'
-        onPress={() => console.log('pressed')}
-        exerciseCountType='Reps'
-        onUpdateCount={(change) => console.log(`update ${change}`)}
-        onDelete={() => console.log('pressed delete')}
-        onCopy={() => console.log('pressed copy')}
-        onDrag={() => console.log('pressed drag')}
-      />
+        {/* Stats Grid */}
+        <View className="flex-row gap-4 pb-10 border-b-[0.5px] border-[#D4D6DD]">
+          <View className="flex-1 bg-bbam-back-card p-4 rounded-3xl items-center justify-center h-32">
+            <MaterialCommunityIcons name='arm-flex' size={24} color="#585AD1" />
+            <Text className="text-m3-title-small font-bold mt-2">{`${totalWorkouts} Workouts`}</Text>
+            <Text className="text-m3-title-small font-bold text-bbam-text-light">Completed</Text>
+          </View>
+          <View className="flex-1 bg-bbam-back-card p-4 rounded-3xl items-center justify-center h-32">
+            <Ionicons name="time" size={24} color="#585AD1" />
+            <Text className="text-m3-title-small font-bold mt-2">{`${totalTimeSpent} Hours`}</Text>
+            <Text className="text-m3-label-medium text-bbam-text-light">Time Spent</Text>
+          </View>
+        </View>
 
-      <Button title='Go to Login Screen' variant='primary' onPress={() => navigation.navigate('Login')} />
+        {/* Workouts Section */}
+        <Text className="text-m3-title-medium font-bold mt-10 mb-5 ml-2 text-bbam-text-main">
+          Your Workouts
+        </Text>
+        <View className='flex-1 max-h-64'>
+          <ScrollView
+            vertical
+            alwaysBounceVertical={false}
+            contentContainerClassName='flex-col gap-2 w-full'
+          >
+            {workoutPlans.map((workout) => (
+              <CardItem 
+                key={workout.id}
+                title={workout.name}
+                subtitle={`${workout.totalSteps} steps, ${workout.avgDuration} mins`}
+                variant='workoutDisplay'
+                onPress={navigateToWorkoutDetails}
+              />
+            ))}
+          </ScrollView> 
+        </View>
+        
+        {/* Add New Button */}
+        <View className='absolute bottom-0 left-6 right-6'>
+          <Button
+            title={(
+              <View className='flex-row items-end justify-center gap-3'>
+                <Ionicons name='add' size={24} color='white' />
+                <Text className='text-m3-body-large font-bold text-white'>Add New</Text>
+              </View>
+            )} 
+            variant='primary'
+            className='h-16'
+            onPress={navigateToCreateWorkout}
+          />
+        </View>
+
+      </View>
     </View>
   );
-}
- 
+};
+
 export default HomeScreen;
